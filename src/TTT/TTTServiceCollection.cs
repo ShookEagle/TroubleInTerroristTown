f3230810ebc44adb6680b4693412f8f0dca6768f
ShookEagle <shookeagle@gmail.com>
2025-07-28 11:02:09 -0500

    Update Service Collection

--- src/TTT/TTTServiceCollection.cs
+++ src/TTT/TTTServiceCollection.cs
@@ -1,7 +1,13 @@
 using CounterStrikeSharp.API.Core;
 using Microsoft.Extensions.DependencyInjection;
 using TTT.English.Generic;
+using TTT.English.Logs;
+using TTT.English.Roles;
 using TTT.Formatting.Views;
+using TTT.Formatting.Views.Logs;
+using TTT.Formatting.Views.Roles;
+using TTT.Logs;
+using TTT.Roles;
 
 namespace TTT;
 
@@ -13,5 +19,10 @@ public class TTTServiceCollection : IPluginServiceCollection<TTT>
     /// <inheritdoc />
     public void ConfigureServices(IServiceCollection serviceCollection) {
         serviceCollection.AddSingleton<IGenericCmdLocale, GenericCmdLocale>();
+        serviceCollection.AddSingleton<ILogLocale, LogLocale>();
+        serviceCollection.AddSingleton<IRoleLocale, RoleLocale>();
+
+        serviceCollection.AddTTTLogs();
+        serviceCollection.AddTTTRoles();
     }
 }
