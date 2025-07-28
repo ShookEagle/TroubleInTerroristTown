80f44dab69e51e222d0024f31d5124ef746de796
ShookEagle <shookeagle@gmail.com>
2025-07-28 10:47:29 -0500

    Apply Code Reformatting

--- mod/TTT.Roles/Listeners/RoleAssignmentListener.cs
+++ mod/TTT.Roles/Listeners/RoleAssignmentListener.cs
@@ -7,34 +7,41 @@ using TTT.Public.Utils;
 
 namespace TTT.Roles.Listeners;
 
-public class RoleAssignmentListener(IRoleFactory roleFactory, IPlayerStateFactory playerStateFactory) : IPluginBehavior {
+public class RoleAssignmentListener(IRoleFactory roleFactory,
+  IPlayerStateFactory playerStateFactory, ICoroutines coroutines)
+  : IPluginBehavior {
   private readonly IPlayerState<RoleState> roleState =
     playerStateFactory.Round<RoleState>();
-  
+
   [GameEventHandler]
   public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info) {
+    coroutines.Round(assignRoles);
+    return HookResult.Continue;
+  }
+
+  private void assignRoles() {
     var players  = PlayerUtil.GetAlive().ToList();
     var shuffled = players.OrderBy(_ => Guid.NewGuid()).ToList();
     var assigned = new HashSet<CCSPlayerController>();
-    
+
     var totalPlayers = players.Count;
     var allRoles = Enum.GetValues<RoleType>()
      .Select(rt => roleFactory.Get(rt))
      .Where(r => r.PlayerRatio is not -1)
      .ToList();
-    
+
     foreach (var role in allRoles) {
-      var ratioCount = role.PlayerRatio > 0 ? totalPlayers / role.PlayerRatio : int.MaxValue;
+      var ratioCount = role.PlayerRatio > 0 ?
+        totalPlayers / role.PlayerRatio :
+        int.MaxValue;
       var maxCount = role.MaxCount > 0 ? role.MaxCount : int.MaxValue;
-      var count = Math.Min(ratioCount, maxCount);
-      
+      var count    = Math.Min(ratioCount, maxCount);
+
       foreach (var player in shuffled.Except(assigned).Take(count)) {
         roleState.Get(player).Type = role.Type;
         role.OnAssigned(player);
         assigned.Add(player);
       }
     }
-    
-    return HookResult.Continue;
   }
 }
