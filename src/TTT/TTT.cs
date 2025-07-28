ecef853763337c6df0426c851952015734b0b527
ShookEagle <shookeagle@gmail.com>
2025-07-28 11:38:20 -0500

    Update Builder

--- src/TTT/TTT.cs
+++ src/TTT/TTT.cs
@@ -9,21 +9,22 @@ using TTT.Public.Behaviors;
 namespace TTT;
 
 public class TTT : BasePlugin {
-  private readonly IServiceProvider _provider;
-  private IReadOnlyList<IPluginBehavior>? _extensions;
-  private IServiceScope? _scope;
+  private readonly IServiceProvider provider;
+  private IReadOnlyList<IPluginBehavior>? extensions;
+  private IServiceScope? scope;
 
   /// <summary>
   ///   The TTT plugin.
   /// </summary>
   /// <param name="provider"></param>
-  public TTT(IServiceProvider provider) { this._provider = provider; }
+  public TTT(IServiceProvider provider) { this.provider = provider; }
 
   public override string ModuleName => "TTT";
 
-  public override string ModuleVersion => "1.0.0";
+  public override string ModuleVersion 
+    => $"{GitVersionInformation.SemVer} ({GitVersionInformation.ShortSha})";
 
-  public override string ModuleAuthor => "ShookEagle";
+  public override string ModuleAuthor => "EdgeGamers Development";
 
   /// <inheritdoc />
   public override void Load(bool hotReload) {
@@ -34,14 +35,14 @@ public class TTT : BasePlugin {
     //  Load Managers
     Logger.LogInformation("[TTT] Loading...");
 
-    _scope = _provider.CreateScope();
-    _extensions = _scope.ServiceProvider.GetServices<IPluginBehavior>()
+    scope = provider.CreateScope();
+    extensions = scope.ServiceProvider.GetServices<IPluginBehavior>()
      .ToImmutableList();
 
     Logger.LogInformation("[TTT] Found {@BehaviorCount} behaviors.",
-      _extensions.Count);
+      extensions.Count);
 
-    foreach (var extension in _extensions) {
+    foreach (var extension in extensions) {
       //	Register all event handlers on the extension object
       RegisterAllAttributes(extension);
 
@@ -54,11 +55,11 @@ public class TTT : BasePlugin {
 
     //	Expose the scope to other plugins
     Capabilities.RegisterPluginCapability(API.Provider, () => {
-      if (_scope == null)
+      if (scope == null)
         throw new InvalidOperationException(
           "TTT does not have a running scope! Is the TTT plugin loaded?");
 
-      return _scope.ServiceProvider;
+      return scope.ServiceProvider;
     });
 
     base.Load(hotReload);
@@ -68,14 +69,14 @@ public class TTT : BasePlugin {
   public override void Unload(bool hotReload) {
     Logger.LogInformation("[TTT] Shutting down...");
 
-    if (_extensions != null)
-      foreach (var extension in _extensions)
+    if (extensions != null)
+      foreach (var extension in extensions)
         extension.Dispose();
 
     //	Dispose of original extensions scope
     //	When loading again we will get a new scope to avoid leaking state.
-    _scope?.Dispose();
-    _scope = null;
+    scope?.Dispose();
+    scope = null;
 
     base.Unload(hotReload);
   }
